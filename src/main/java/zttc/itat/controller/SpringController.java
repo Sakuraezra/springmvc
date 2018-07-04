6dfb07769fb950ff33b11f939e1e6d938a4392a3
fengwenping <244160190@163.com>
2018-07-04 23:34:09 +0800

    修改的地方很多,需要一一去核对,主要点: 1.maven重复以来的包进行了修正,通过间接以来的方式即可; 2.增加mysql的以来,把ojdbc6的jar包也通过maven进行以来; 3.把是spring的所有以来通过统一的版本进行控制; 4.去掉冲突的包,主要是jstl(1.2)与standard冲突; 5.去掉log4j 1.*版本对应的包,统一升级为2.*版本的包,并且配置了统一的log4j.xml文件进行日志的打印; 6.数据库配置采用统一的陪你文件application.properties文件进行管理,并对配置文件实用spring进行管理; 7.SystemContextFilter里面的把所有请求进行了拦截,所以出现了资源访问总是找不到; 8.把spring和springMVC的配置文件进行了分离,spring和springMVC本来就是两个容器; 9.controller中存在重复的mapping,已经删除,主要是/路径的;

--- src/main/java/zttc/itat/controller/SpringController.java
+++ src/main/java/zttc/itat/controller/SpringController.java
@@ -2,56 +2,63 @@ package zttc.itat.controller;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-
 @Controller
 public class SpringController {
-	
-	
-	//RequesMapping表示用哪个URL来对应
-	@RequestMapping("/hello")	
-	public String hello() {
-			return "hello";
-		}
-	
-	//使用模型驱动 传值
-	@RequestMapping({"/modelvalue","/"})
-    public String modelvalue(String username,Model model) {
-		System.out.println("modelvalue");
-	        model.addAttribute("username",username);
-	        //   model.addAttribute(username);
-	        //此时用那个作为Key？它默认使用对象类型为： Key--> 键值对
-	    
-	        //   model.addAttribute(User());
-	        //此时用那个作为Key？它默认使用对象类型为： Key--> addAttribute(user, new User())
-	    
-	        
-	        return "modelvalue";
-	    }
-	
-	
-	
-	//传值操作 使用@RequestParam
-	@RequestMapping({"/welcome","/"})
-	public String welcome(@RequestParam ("username") String username) {
-		System.out.println("welcome");
-		System.out.println(username);
-		return "welcome";
-	}
-	
-	//传值操作   使用map
-		@RequestMapping({"/mapvalue","/"})
-		public String mapvalue(String username ,Map<String,Object> context) {
-			System.out.println("mapvalue");
-			context.put("username", username);
-			System.out.println(username);
-			return "mapvalue";
-		}
-		
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringController.class);
+
+    //RequesMapping表示用哪个URL来对应
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    //使用模型驱动 传值
+    @RequestMapping({"/modelvalue"})
+    public String modelvalue(String username, Model model) {
+        System.out.println("modelvalue");
+        model.addAttribute("username", username);
+        //   model.addAttribute(username);
+        //此时用那个作为Key？它默认使用对象类型为： Key--> 键值对
+
+        //   model.addAttribute(User());
+        //此时用那个作为Key？它默认使用对象类型为： Key--> addAttribute(user, new User())
+
+
+        return "modelvalue";
+    }
+
+
+    //传值操作 使用@RequestParam
+    @RequestMapping("/welcome")
+    public String welcome(@RequestParam("username") String username) {
+        System.out.println("welcome");
+        System.out.println(username);
+        return "welcome";
+    }
+
+    //传值操作   使用map
+    @RequestMapping({"/mapvalue"})
+    public String mapvalue(String username, Map<String, Object> context) {
+        System.out.println("mapvalue");
+        context.put("username", username);
+        System.out.println(username);
+        return "mapvalue";
+    }
+
+    @GetMapping(value = {"login"})
+    public String loginPage() {
+        return "login";
+    }
+
 }
 
